b07c4def0adf5a0c4aa0042609429fcdfba281c5
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-08-28 20:02:51 +0200

    Icon

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-cd /D D:\Elsacoding\Projects\OpenNotes\OpenNotes\OpenNotes
+cd /D D:\Elsacoding\Projects\OpenNotes\OpenNotes\OpenNotes\OpenNotes
 "D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" OpenNotes.csproj
+copy icon.ico OpenNotes\bin\Debug\
